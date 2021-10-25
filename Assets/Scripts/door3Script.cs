9ca03063b8d9b767ed354edd3715bbd9a04c572e
ernabalayan <69862007+ernabalayan@users.noreply.github.com>
2021-10-25 01:18:24 -0400

    added key 3 script and boss script

--- Assets/Scripts/door3Script.cs
+++ Assets/Scripts/door3Script.cs
@@ -1,18 +1,58 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class door3Script : MonoBehaviour
 {
+    public GameObject key;
+    public Collider2D keyCollider;
+    public SpriteRenderer spriteRenderer;
+    public BoxCollider2D boxCollider;
+    public SpriteRenderer keySpriteRenderer;
+    public PolygonCollider2D keySpriteCollider;
+    public bool doorOpen;
+    public Text _text;
+    public Image textbox;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _text.GetComponent<Text>().enabled = false;
+        textbox.GetComponent<Image>().enabled = false;
+        doorOpen = false;
+        boxCollider.enabled = true;
+        spriteRenderer.enabled = true;
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        if (key.GetComponent<key3>().hasKey == false)
+        {
+            _text.GetComponent<Text>().enabled = true;
+            _text.text = "Find gold key to unlock";
+            textbox.GetComponent<Image>().enabled = true;
+        }
+
+        if (key.GetComponent<key3>().hasKey)
+        {
+            doorOpen = true;
+            boxCollider.enabled = false;
+            spriteRenderer.enabled = false;
+        }
+        if (doorOpen)
+        {
+            Destroy(key);
+
+        }
     }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        _text.GetComponent<Text>().enabled = false;
+        textbox.GetComponent<Image>().enabled = false;
+    }
+    // Update is cal
 }
